6c4af4947ab0d2ce04944748696954fabe2df114
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2015-05-04 17:17:30 +0100

    First commit

---
+++ PartInvites/PartInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using PartInvites.Models;
+
+namespace PartInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // There is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ PartInvites/PartInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify wether you will be attending")]
+        public bool? WillAttend { get; set; }
+    }
+}
