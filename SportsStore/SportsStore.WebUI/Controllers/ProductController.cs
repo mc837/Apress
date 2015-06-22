9d2d047e37794280723ad075baae45d41379f884
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2015-06-22 20:30:38 +0100

    SportsStore (Chapter7) list products via a moq object

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository _repository;
+
+        public ProductController(IProductsRepository productRepository)
+        {
+            this._repository = productRepository;
+        }
+
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory: DefaultControllerFactory
+    {
+        private readonly IKernel _ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) _ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //Add bindings here
+            var mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surf Board", Price = 75},
+                new Product {Name = "Running Shoes", Price = 95}
+            }.AsQueryable());
+
+            _ninjectKernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+        }
+    }
+}
